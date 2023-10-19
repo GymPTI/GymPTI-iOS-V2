5e1725cc62919daa933f0267aeab74c83ead4bf3
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-10-19 14:32:25 +0900

    ADD: ImagePickerView 추가

---
+++ GymPTI-V2/GymPTI-V2/Feature/Profile/EditInfo/ImagePickerView.swift
@@ -0,0 +1,55 @@
+//
+//  ImagePickerView.swift
+//  GymPTI-V2
+//
+//  Created by 이민규 on 10/19/23.
+//
+
+import SwiftUI
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    
+    @Binding var result: String
+    @Binding var selectedImage: UIImage
+    @Environment(\.presentationMode)  var presentationMode
+    
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePickerView>) -> UIImagePickerController {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = context.coordinator
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = sourceType
+        
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePickerView>) {
+        
+    }
+}
+
+
+final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    let parent: ImagePickerView
+    
+    init(_ parent: ImagePickerView) {
+        self.parent = parent
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            parent.selectedImage = selectedImage
+        }
+        parent.presentationMode.wrappedValue.dismiss()
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        parent.presentationMode.wrappedValue.dismiss()
+    }
+}
