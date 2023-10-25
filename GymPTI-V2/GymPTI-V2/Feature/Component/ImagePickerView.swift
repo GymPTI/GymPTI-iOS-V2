0a351d4ce2c7be823e4c144b5e64768c18284513
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-10-25 11:34:33 +0900

    FIX: 프로젝트 안되는 오류 수정

--- GymPTI-V2/GymPTI-V2/Feature/Component/ImagePickerView.swift
+++ GymPTI-V2/GymPTI-V2/Feature/Component/ImagePickerView.swift
@@ -8,51 +8,47 @@
 import SwiftUI
 
 struct ImagePickerView: UIViewControllerRepresentable {
-    
+
     @Binding var result: String
     @Binding var selectedImage: UIImage
     @Environment(\.presentationMode)  var presentationMode
-    
+
     var sourceType: UIImagePickerController.SourceType = .photoLibrary
-    
+
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    
+
     func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePickerView>) -> UIImagePickerController {
-        
+
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = context.coordinator
         imagePicker.allowsEditing = true
         imagePicker.sourceType = sourceType
-        
+
         return imagePicker
     }
-    
+
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePickerView>) {
-        
+
     }
 }
 
 
 final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
     let parent: ImagePickerView
-    
+
     init(_ parent: ImagePickerView) {
         self.parent = parent
     }
-    
+
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        
-        if let possibleImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
-            parent.selectedImage = possibleImage
-        } else if let possibleImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            parent.selectedImage = possibleImage
+        if let selectedImage = info[.originalImage] as? UIImage {
+            parent.selectedImage = selectedImage
         }
         parent.presentationMode.wrappedValue.dismiss()
     }
-    
+
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         parent.presentationMode.wrappedValue.dismiss()
     }
